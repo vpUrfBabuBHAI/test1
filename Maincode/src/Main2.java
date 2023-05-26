1b4d5e95c215f235e8b924bd85986e2996c284b5
sdac-18 <sdac-18@sdac-18-PC>
2023-05-26 03:51:15 -0700

    update2.0

--- Maincode/src/Main2.java
+++ Maincode/src/Main2.java
@@ -1,7 +1,6 @@
 
 public class Main2 {
-public static void main(String[] args) {
-	System.out.println("update2.0");
-	System.out.println("update2.0");
+	public static void main(String[] args) {
+		System.out.println("update2.0");
 }
 }
