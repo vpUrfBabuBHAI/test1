20f8cdfbd2c84db98b8927d526e023c2786739e3
sdac-18 <sdac-18@sdac-18-PC>
2023-05-26 03:38:58 -0700

    update1.0

--- Maincode/src/main1/Main1.java
+++ Maincode/src/main1/Main1.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("update1.0");
 	System.out.println("update1.0");
 	System.out.println("update1.0");
+	System.out.println("update1.0");
 }
 }
